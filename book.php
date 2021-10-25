ad1c0a1f71439901f7ff19b0c648b46090f574c1
Cat <71066639+nekocats@users.noreply.github.com>
2021-10-25 11:00:08 +0300

    added connection.php and added config file template
    cat

--- book.php
+++ book.php
@@ -1,33 +1,13 @@
 <?php
 
-require_once "config.php";
+require_once 'connection.php';
 
-$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
-$options = [
-    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    PDO::ATTR_EMULATE_PREPARES   => false,
-];
-try {
-     $pdo = new PDO($dsn, $user, $pass, $options);
-} catch (\PDOException $e) {
-     throw new \PDOException($e->getMessage(), (int)$e->getCode());
-}
+$stmt = $pdo ->prepare ('SELECT * FROM books WHERE id=:id');
 
-echo $_GET["id"];
-
-$stmt = $pdo->prepare('SELECT * FROM books where id =:id ');
-$stmt->execute([':id' => $_GET["id"]]);
+$stmt-> execute([':id' => $_GET['id']]);
 $book = $stmt->fetch();
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?=$book["title"]?></title>
-</head>
-<body>
-    <h1><?=$book["title"]?></h1>
-</body>
+<html>
+<p>Raamatu nimi: <?php echo $book['title']; ?></p>
+<p>Väljalaske aasta: <?php echo $book['release_date']; ?></p>
 </html>
---
+++ config template.php
@@ -0,0 +1,7 @@
+<?php
+
+$host = 'hostname here';
+$db   = 'database name here';
+$user = 'username here';
+$pass = 'passcode here';
+$charset = 'utf8mb4';
