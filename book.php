f30a2a109b39e85c164934bb57d070d8c011db78
Cat <71066639+nekocats@users.noreply.github.com>
2021-10-28 11:24:43 +0300

    Fixed bugs and added delete functionality, edit works too!
    cat

--- book.php
+++ book.php
@@ -1,46 +1,44 @@
 <?php
 
 require_once 'connection.php';
-
+$bookId = $_GET['id'];
 $stmt = $pdo ->prepare ('SELECT * FROM books b LEFT JOIN book_authors ba ON b.id=ba.book_id LEFT JOIN authors a ON a.id=ba.author_id WHERE b.id=:id');
 
 $stmt-> execute([':id' => $_GET['id']]);
 $book = $stmt->fetch();
 ?>
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <link rel="stylesheet" type="text/css" href="style.css"/>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100&display=swap" rel="stylesheet"> 
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@600&family=Nunito:wght@700&display=swap" rel="stylesheet">
+    </head>
 
-<html>
-<head>
-    <link rel="stylesheet" type="text/css" href="style.css"/>
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100&display=swap" rel="stylesheet"> 
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@600&family=Nunito:wght@700&display=swap" rel="stylesheet">
-</head>
+    <h1><?php echo $book['title']; ?></h1>
 
-<h1><?php echo $book['title']; ?></h1>
+    <div class="kaanepilt">
+        <img src="<?php echo $book['cover_path']; ?>" alt="Raamatu kaas"> <br>
+    </div>
+    <body>
+        <div class="tekst">
+            <p>Väljalaske aasta: <?php echo $book['release_date']; ?></p>
+            <p>Autor: <?php echo $book['first_name']; ?> <?php echo $book['last_name']; ?></p>
+            <p>Hind: <?php echo round($book['price'],2) ?></p>
+        </div>
+    
+    <div class="neko">
+        <a href="edit.php?id=<?=$bookId?>">
+            Muuda
+        </a>
+        <a href="delete.php?id=<?=$bookId?>">Kustuta</a>
+    </div>
+   
+    </body>
 
-<div class="kaanepilt">
-    <img src="<?php echo $book['cover_path']; ?>" alt="Raamatu kaas"> <br>
-</div>
 
-<div class="tekst">
-    <p>Väljalaske aasta: <?php echo $book['release_date']; ?></p>
-
-    <p>Keel: <?php echo $book['language']; ?></p>
-
-    <p>Kirjeldus: <?php echo $book['summary']; ?></p>
-
-    <p>Autor: <?php echo $book['first_name']; ?> <?php echo $book['last_name']; ?></p>
-
-    <p>Lehti: <?php echo $book['pages']; ?></p>
-
-    <p>Hind: <?php echo $book['price']; ?></p>
-
-</div>
-
-<div class="neko">
-    <a href="edit.php?id=<?php echo $book['id']; ?>">Muuda</a>
-</div>
 
 
 </html>
--- edit.php
+++ edit.php
@@ -3,41 +3,41 @@
 require_once 'connection.php';
 
 $bookId = $_GET['id'];
-
 if ( isset($_POST['save']) ) {
-
     $stmt = $pdo->prepare('UPDATE books SET title=:title WHERE id=:id');
-    $stmt->execute([':id' => $bookId, ':title' => $_REQUEST['title']]);
+    $stmt->execute([':id' => $bookId, ':title' => $_POST['title']]);
 
     header('Location: book.php?id=' . $bookId);
-
 }
 
 $stmt = $pdo ->prepare ('SELECT * FROM books b LEFT JOIN book_authors ba ON b.id=ba.book_id LEFT JOIN authors a ON a.id=ba.author_id WHERE b.id=:id');
 
-$stmt->execute([':id' => $bookId]);
+$stmt-> execute([':id' => $_GET['id']]);
 $book = $stmt->fetch();
-
 ?>
-
+<html>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Muuda</title>
     <link rel="stylesheet" type="text/css" href="style.css"/>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100&display=swap" rel="stylesheet"> 
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@600&family=Nunito:wght@700&display=swap" rel="stylesheet">
 </head>
-
 <body>
-    <form action="edit.php?id=<?php $bookId; ?>" method="post">
-        <input type="text" name="title" value="<?php $book['title']; ?>">
-        <input type="submit" name="save" value="Salvesta">
-    </form>
+
+<form action="edit.php?id=<?= $bookId; ?>" method="post">
+    <input class="input" type="text" name="title" value="<?= $book ['title']; ?>">
+    <input class="editing" type="submit" name="save" value="Salvesta">
+</form>
+
+    
 </body>
+</html>
+
 </html>
